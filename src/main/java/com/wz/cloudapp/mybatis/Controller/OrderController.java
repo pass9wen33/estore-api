57b6fd69471cc23fa0afdb36caac2f0f4b03d19c
wen.weiwei <wen.weiwei@opg.cn>
2024-06-02 00:59:37 +0800

    add query list api

--- src/main/java/com/wz/cloudapp/mybatis/Controller/OrderController.java
+++ src/main/java/com/wz/cloudapp/mybatis/Controller/OrderController.java
@@ -5,6 +5,7 @@ import com.wz.cloudapp.mybatis.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,10 @@ public class OrderController {
     @GetMapping("/list")
     public List<Order> getAll() {
         return orderDao.selectAll();
+    }
+
+    @GetMapping("query")
+    public Order getListById(@RequestParam("id")String id) {
+        return orderDao.getOrderById(id);
     }
 }
