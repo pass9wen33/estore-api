66293846583f5a33290f5244b76c2a827cd8907e
wen.weiwei <wen.weiwei@opg.cn>
2024-06-01 23:36:52 +0800

    修改order/list接口路径

--- src/main/java/com/wz/cloudapp/mybatis/Controller/OrderController.java
+++ src/main/java/com/wz/cloudapp/mybatis/Controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Order")
+@RequestMapping("/estore-api/order")
 public class OrderController {
     @Autowired
     private OrderDao orderDao;
